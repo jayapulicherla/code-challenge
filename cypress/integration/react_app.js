1e1db4f89e1240069589aefedd76ff9a35828b43
suryagodavarthi <suryagodavarthi@suryas-air.lan>
2020-05-01 11:30:29 -0700

    change

--- cypress/integration/react_app.js
+++ cypress/integration/react_app.js
@@ -3,7 +3,7 @@ describe('React App Page Test', () => {
 		cy.visit('http://localhost:3000')
 		cy.contains('Hello World!')
 		cy.contains('The world is green!')
-		cy.log('Test Complete')
+		cy.log('Test Complete--')
 		
 	})
 })
