9b9a8b9e086921f60f79b1a9c79edd2b7c987a8b
suryagodavarthi <suryagodavarthi@suryas-air.lan>
2020-05-04 06:19:26 -0700

    j

--- cypress/integration/react_app.js
+++ cypress/integration/react_app.js
@@ -1,7 +1,7 @@
 describe('React App Page Test', () => {
 	it('finds the content "type"', () => {
 		cy.visit('http://localhost:3000')
-		cy.contains('Hello World!')
+		cy.contains('Hello Word!')
 		cy.contains('The world is green!')
 		cy.log('Test Complete--')
 		
